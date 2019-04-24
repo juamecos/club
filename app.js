00fbdb02a3d15deb06b6e61e91c740a9c65d2984
juamecos <juamecos@gmail.com>
2019-04-24 21:05:47 +0200

    added latest news

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-document.querySelector('.button1').addEventListener('click', init);
+// Lastest news
 
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ibW3g04xoA491mT_NTaWSsFHXbFixHKd0SFP3qg2pNo/edit?usp=sharing';
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1wN1bHqX8zkIS2f5DusWJVuuLwhoaNiA3D6lSr0J8o0E/edit?usp=sharing';
 
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
@@ -9,26 +9,56 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ibW3g04xoA49
   }
 
   function showInfo(data, tabletop) {
-    // alert('Successfully processed!')
-    const card1 = document.querySelector(".latest-news__news--1");
-    card1.innerHTML = `
-    <div class="news">
-                        <div class="news__card">
-                            <img src=${data[1].picturelarge} alt="Muscari neglectum" class="news__img">
-                            <div class="news__text-box u-padding-vertical-medium">
-                                <h4 class="news__title">${data[1].family}</h4>
-                                <h5 class="news__subtitle">${data[1].commonName}</h5>
-                                <p class="news__paragraph--card">${data[1].habitat}&nbsp;<a href="#" class="btn btn--text">Read more &rarr;</a></p>
-                                
-                                <div class="news__info">
-                                    <span class="news__author">J. J. Mena</span>
-                                    <span class="news__date">17 Apr 2019</span>
-                                </div>                                
-                            </div>
-                        </div>
+    // Sort the articles by date
+    const sortedByDate = data.sort((a, b) =>  new Date(b.date) - new Date(a.date));
+
+    // filter published articles
+    const published = sortedByDate.filter(el => el.published  === "TRUE");
+   
+    // pic only the latest 4 articles
+    const latestArticles = published.slice(0,4);
+    const wrapper = document.querySelector(".latest-news__wrapper");
+    console.log(latestArticles);
+    latestArticles.forEach((item, index) => {
+        let i = index + 1;
+        let output = "";
+        if (i !== 1) {
+          item.paragraph1 = item.paragraph1.substring(0, 119);
+          output = `
+        <div class="article article--${i}">
+                <div class="article__container">
+                    <h2 class="article__title article__title--${i}">${item.title}</h2>
+                    <h3 class="article__subtitle article__subtitle--${i}">${item.subtitle}</h3>
+                    <div class="article__info">
+                        <p class="article__author article__author--${i}">${item.author}</p>
+                        <time class="article__date article__date--${i}">${item.date}</time>
                     </div>
-    `;
-    console.log(data[1], typeof data[1].picturelarge);
+                    <p class="article__text article__text--${i}">${item.paragraph1}&nbsp;&nbsp;<a href="#" class="btn btn--txt btn--txt--${i}">Read more &RightArrow;</a></p>
+                    `
+        } else {
+          output = `
+        <div class="article article--${i}">
+                <div class="article__container">
+                    <h2 class="article__title article__title--${i}">${item.title}</h2>
+                    <h3 class="article__subtitle article__subtitle--${i}">${item.subtitle}</h3>
+                    <div class="article__info">
+                        <p class="article__author article__author--${i}">${item.author}</p>
+                        <time class="article__date article__date--${i}">${item.date}</time>
+                    </div>
+                    <img src=${item.image} alt="" class="article__img">
+                    <p class="article__text article__text--${i}">${item.paragraph1}</p>
+                    <p class="article__text article__text--${i}">${item.paragraph2}&nbsp;&nbsp;<a href="#" class="btn btn--txt btn--txt--${i}">Read more &RightArrow;</a></p>
+                    
+                </div>
+            </div>
+        `; 
+        }
+        
+        wrapper.innerHTML += output; 
+    });
+
+    
+
   }
 
 window.addEventListener('DOMContentLoaded', init)
